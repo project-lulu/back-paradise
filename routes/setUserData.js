3ce0efaea0c9245e4827153bceaa99eb2409a541
Emilia Luminé <eqilia@national.shitposting.agency>
2024-09-10 10:22:04 +0200

    Add routes/setUserData.js
    FINALLYYYYY
    
    Signed-off-by: Emilia Luminé <eqilia@national.shitposting.agency>

---
+++ routes/setUserData.js
@@ -0,0 +1,40 @@
+module.exports = {
+	method: 'post',
+	path: '/users/*',
+	async exec(req, res) {
+		const uid = req.path.split('/')[2];
+		let body;
+		let userData;
+
+		try {
+			body = JSON.parse(req.body);
+		} catch (e) {
+			res.status(400).send(JSON.stringify({
+				error: 1
+			}));
+			return;
+		}
+
+		if (
+			typeof body.discriminator !== 'string' ||
+			typeof body.username !== 'string' ||
+			typeof body.passhash !== 'string' ||
+			typeof body.disabled !== 'number'
+		) {
+			res.status(400).send(JSON.stringify({
+				error: 2
+			}));
+			return;
+		}
+
+		await globalThis.db.set(`user_${uid}`, JSON.stringify({
+			discriminator: body.discriminator,
+			username: body.username,
+			passhash: body.passhash,
+			disabled: body.disabled
+		}));
+
+		res.status(204).end();
+		return;
+	}
+}
